50b92cddfa525aab2791a2cec35b325aa2c7afc0
fenil <fenil12vaghasiya@gmail.com>
2024-09-14 01:35:44 -0400

    restored deleted repo

---
+++ client/src/redux/slices/apiSlice.js
@@ -0,0 +1,72 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+
+export const apiSlice = createApi({
+  reducerPath: 'api',
+  baseQuery: fetchBaseQuery({
+    baseUrl: 'http://localhost:8800/api',
+    prepareHeaders: (headers, { getState }) => {
+      // Retrieve the token from the state
+      const token = getState().auth.user?.token;
+
+      // If the token exists, include it in the headers for `createTask`
+      if (token) {
+        headers.set('authorization', `${token}`);
+      }
+      return headers;
+    },
+  }),
+  
+  endpoints: (builder) => ({
+    login: builder.mutation({
+      query: (credentials) => ({
+        url: '/user/login',
+        method: 'POST',
+        body: credentials,
+      }),
+    }),
+    register: builder.mutation({
+      query: (userData) => ({
+        url: '/user/register',
+        method: 'POST',
+        body: userData,
+      }),
+    }),
+    updateUserProfile: builder.mutation({
+      query: (profileData) => ({
+        url: '/user/profile',
+        method: 'PUT',
+        body: profileData,
+      }),
+    }),
+    createTask: builder.mutation({
+      query: (taskData) => ({
+        url: '/task/create',
+        method: 'POST',
+        body: taskData,
+      }),
+    }),
+    updateTask: builder.mutation({
+      query: (taskData) => ({
+        url: '/task/update/:id',
+        method: 'PUT',
+        body: taskData,
+      }),
+    }),
+    GetTask: builder.mutation({
+      query: (getTaskData) => ({
+        url: '/task',
+        method: 'GET',
+        body: getTaskData,
+      }),
+    }),
+  }),
+});
+
+export const {
+  useLoginMutation,
+  useRegisterMutation,
+  useUpdateUserProfileMutation,
+  useCreateTaskMutation,
+  useUpdateTaskMutation,
+  useGetTaskMutation,
+} = apiSlice;
